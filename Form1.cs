a4bba0b177efbe1652ff1fb83b896fb68c50f42a
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2022-01-31 13:38:26 +0000

    Sending over to be used on laptop
    For some reason DLL causes errors, uploading to repo to try on laptop

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,10 +18,11 @@ namespace CS_GridGame_Team5
             InitializeComponent();
             String name = "Spitfire MK2";
             PlaneType type = PlaneType.fighter;
-            uint altitude = 1;
+            int altitude = 1;
             AmmoType ammo = AmmoType.light;
             Plane spitFireMK2 = new Plane(ref name, ref type, ref altitude, ref ammo);
             Console.WriteLine(spitFireMK2.Name);
+            Console.WriteLine();
         }
 
         private void Form_Game_Load(object sender, EventArgs e)
--- Plane.cs
+++ Plane.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace CS_GridGame_Team5
 {
     class Plane
     {
+        public const string DamageCalcDLL_fp = @"..\..\DLL Workspace\DamageCalDLL_C++\x64\Debug\DamageCalc.dll"; // Define the damage calculation DLL filepath
+
+        [DllImport(DamageCalcDLL_fp, CallingConvention = CallingConvention.Cdecl)] // Import DLL file into class
+        public static extern int calculateDamage(string type, int altitude, string ammoType); // Define function from DLL
+        
+        [DllImport(DamageCalcDLL_fp, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr getVAL(int VAL);
+
         private String name;
         private PlaneType type;
-        private uint altitude;
+        private int altitude;
         private AmmoType ammoType;
 
         public Plane()
@@ -21,7 +30,7 @@ namespace CS_GridGame_Team5
             this.ammoType = AmmoType.light;
         }
 
-        public Plane(ref String name, ref PlaneType type, ref uint altitude, ref AmmoType ammoType)
+        public Plane(ref String name, ref PlaneType type, ref int altitude, ref AmmoType ammoType)
         {
             this.name = name;
             this.type = type;
@@ -29,10 +38,11 @@ namespace CS_GridGame_Team5
             this.ammoType = ammoType;
 
             Console.WriteLine(name + ", " + type + ", " + altitude + ", " + ammoType); // Debug data
+            Console.WriteLine(getVAL(altitude));
         }
 
         public string Name { get => name; set => name = value; }
-        public uint Altitude { get => altitude; set => altitude = value; }
+        public int Altitude { get => altitude; set => altitude = value; }
         internal PlaneType Type { get => type; set => type = value; }
         internal AmmoType AmmoType { get => ammoType; set => ammoType = value; }
     }
