017de7d4561450976a75e710f57639901053b1c0
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2022-02-10 17:09:56 +0000

    Damage calculation re-designed

--- DLL Workspace/CalculateDamageDLL/Calculate.cs
+++ DLL Workspace/CalculateDamageDLL/Calculate.cs
@@ -5,31 +5,74 @@ namespace Calculate
     public class Compute
     {
         // Takes in two integer values that are the altitudes of the plane that is shooting and the target plane, and the ammo type being fired.
-        public static int damageOutput(int ownAltitude, int targetAltitude, byte ammoType)
+        public static int damageOutput(int selfAltitude, byte selfObjectType,  byte ammoType, int range, int targetAltitude, byte targetObjectType)
         {
             int altitudeModifier = 0;
-            int ammoTypeModifier = 1;
+            int rangeModifier = 0;
+            int ammoModifier = 0;
+            float objectDamageModifier = 0;
 
-            if (ammoType == 0) { ammoTypeModifier = 1; }
-            else if (ammoType == 1) { ammoTypeModifier = 2; }
-
-            if (ownAltitude > targetAltitude || ownAltitude < targetAltitude)
+            // Check for altitude and determine altitude modifier.
+            if (selfAltitude != targetAltitude)
             {
-                altitudeModifier = ownAltitude - targetAltitude;
+                altitudeModifier = selfAltitude - targetAltitude;
             }
 
-            // IF potential damage output <= 0 - MISSES SHOTS
-            if ((int)Math.Round(((altitudeModifier + ammoTypeModifier) / 0.5)) <= 0)
+            // Check the range and determine rangeModifier
+            if (range == 1) // One tile away from source.
+            {
+                rangeModifier = 3;
+            }
+            else if (range == 2) // Two tiles away from source.
+            {
+                rangeModifier = 2;
+            }
+            else if (range == 3) // Three tiles away from source.
+            {
+                rangeModifier = 1;
+            }
+
+            // Check ammo type shot from source plane and determine modifier.
+            if (ammoType == 2) // Light rounds
+            {
+                ammoModifier = 1;
+            }
+            else if (ammoType == 3) // Heavy rounds
+            {
+                ammoModifier = 2;
+            }
+
+            // Check object type being shot at.
+            if (targetObjectType == 2) // Fighter plane
+            {
+                objectDamageModifier = 1f;
+            }
+            else if(targetObjectType == 3) // Bomber plane
+            {
+                objectDamageModifier = 0.75f;
+            }
+            else if(targetObjectType == 4 && selfObjectType == 3) // Dam - Only can damage dam if the source plane is a bomber
+            {
+                if (selfAltitude == 1)
+                {
+                    return 100; // Hit
+                }
+                return 0; // Miss
+            }
+
+            if (altitudeModifier == -4) // Misses due to target plane being too high above
             {
                 return 0;
             }
 
-            // OTHERWISE randomsize the damage output
-            // IF output is 0 - MISSES SHOTS
-            Random rand = new Random(); // Random number from a range of numbers. - https://stackoverflow.com/questions/3975290/produce-a-random-number-in-a-range-using-c-sharp - 01/02/2022
-            int randInt = rand.Next(1, (int)Math.Round(((altitudeModifier + ammoTypeModifier) / 0.5))); // Can potentially do damage however can still miss.
-            Console.WriteLine("Randomising..");
-            return randInt;
+            // Round number to nearest integer - https://stackoverflow.com/questions/8844674/how-to-round-to-the-nearest-whole-number-in-c-sharp - 10/02/2022
+            int damagePotential = (int)Math.Round((altitudeModifier + rangeModifier) * (1/(Math.Pow(ammoModifier, objectDamageModifier))));
+
+            // Randomise number in range - https://stackoverflow.com/questions/3975290/produce-a-random-number-in-a-range-using-c-sharp - 10/02/2022
+            Random random = new Random();
+            int damageOutput = random.Next(0, damagePotential);
+
+            return damageOutput;
         }
     }
 }
